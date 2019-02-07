2b95f4c44d32f1400442734aa63ab5a8e44dd84a
Brandon Park <brandonpark206@gmail.com>
2019-02-07 10:50:54 -0800

    fixing ajax form bug that allows bots to send spam emails

--- js/ajax-form.js
+++ js/ajax-form.js
@@ -1,4 +1,6 @@
-$(function() {
+$(document).ready(sendForm());
+
+$(sendForm function() {
 
 	// Get the form.
 	var form = $('#contact-form');
@@ -15,34 +17,72 @@ $(function() {
 		var formData = $(form).serialize();
 
 		// Submit the form using AJAX.
-		$.ajax({
-			type: 'POST',
-			url: $(form).attr('action'),
-			data: formData
-		})
-		.done(function(response) {
-			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('error');
-			$(formMessages).addClass('success');
+		if(form_validate()) {
+			$.ajax({
+				type: 'POST',
+				url: $(form).attr('action'),
+				data: formData
+			})
+			.done(function(response) {
+				// Make sure that the formMessages div has the 'success' class.
+				$(formMessages).removeClass('error');
+				$(formMessages).addClass('success');
 
-			// Set the message text.
-			$(formMessages).text(response);
+				// Set the message text.
+				$(formMessages).text(response);
 
-			// Clear the form.
-			$('#contact-form input,#contact-form textarea').val('');
-		})
-		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('success');
-			$(formMessages).addClass('error');
+				// Clear the form.
+				$('#contact-form input,#contact-form textarea').val('');
+			})
+			.fail(function(data) {
+				// Make sure that the formMessages div has the 'error' class.
+				$(formMessages).removeClass('success');
+				$(formMessages).addClass('error');
 
-			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
-		});
+				// Set the message text.
+				if (data.responseText !== '') {
+					$(formMessages).text(data.responseText);
+				} else {
+					$(formMessages).text('Oops! An error occured and your message could not be sent.');
+				}
+			});
+		}
+		return false;
 	});
 
 });
+
+form_validate = function () {
+
+        var name = document.forms["contact-form"]["name"].value;
+        var email = document.forms["contact-form"]["email"].value;
+        var phone = document.forms["contact-form"]["subject"].value;
+        var message = document.forms["contact-form"]["message"].value;
+
+        var validationAlert = document.getElementById("formValidationAlerts");
+        var letterOnlyRegExp = /^[a-zA-Z\s]*$/;
+        var emailRegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        // Check if the fields full name, E-mail and message are filled.
+        if ((name == null || name == "") ||
+            (email == null || email == "") ||
+            (message == null || message == "")) {
+            validationAlert.innerHTML = "* Your Full Name, Your E-mail Address and Your Message are Required fields." +
+                " Please fill all of them.";
+            return false;
+        }
+
+        // Check if the full name is valid (English letters only).
+        if (!(name.match(letterOnlyRegExp))) {
+            validationAlert.innerHTML = "* Please Enter a Valid Name (English letters only).";
+            return false;
+        }
+
+        // Check if the E-mail is valid.
+        if (!(email.match(emailRegExp))) {
+            validationAlert.innerHTML = "* Please Enter a Valid E-mail.";
+            return false;
+        }
+
+        return true;
+    };
